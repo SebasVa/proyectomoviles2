8881fa64932c4eff56482aeeba425919724e7041
SebasVA <svalencia310@gmail.com>
2022-09-03 13:02:08 -0500

    Initial commit

---
+++ app/src/main/java/com/example/proyecto2b/MapsPojo.java
@@ -0,0 +1,23 @@
+package com.example.proyecto2b;
+
+public class MapsPojo {
+    private double latitud;
+
+    public double getLatitud() {
+        return latitud;
+    }
+
+    public void setLatitud(double latitud) {
+        this.latitud = latitud;
+    }
+
+    public double getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(double longitud) {
+        this.longitud = longitud;
+    }
+
+    private double longitud;
+}
